e7b1f6e1d54796c487fd2a7a87a267b660a58f96
Rusted2361 <hamzasajid4040@gmail.com>
2024-03-22 14:22:58 +0500

    start of react app todo list

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="container">
+       <div className="bg-red-600">
+         <h1>
+           hey I am red
+         </h1>
+       </div>
+     </div>
+    </>
+  )
+}
+
+export default App
